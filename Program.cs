0f1d7d6404a0bd73e1d89c0e0cd3bfd24f33c141
Vishal Rathore <vshl.rathore87@gmail.com>
2024-08-24 20:15:15 -0500

    Added Properties and Fields with Getters and Setters

--- Program.cs
+++ Program.cs
@@ -1,64 +1,25 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System;
 
 namespace dotNetPlayground
 {
-    public class Program
+    public class Computer
     {
-        protected Program() { }
+        public string Motherboard { get; set; } = string.Empty;
+        public int CPUCores { get; set; } = 0;
+        public bool HasWifi { set; get; } = false;
+        public bool HasLTE { get; set; } = false;
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; } = string.Empty;
+    }
 
-        private static int GetSum()
-        {
-            int[] intsToCompress = { 10, 15, 20, 25, 30, 12, 34 };
-            int totalValue = 0;
-            foreach (int intForCompression in intsToCompress)
-            {
-                totalValue += intForCompression;
-            }
-            return totalValue;
-        }
-
+    public static class Program
+    {
         public static void Main(string[] args)
         {
-            // Arrays
-            string[] myGrocceryArray = new string[2];
-
-            myGrocceryArray[0] = "First Array Value";
-            Console.WriteLine(myGrocceryArray[0]);
-            Console.WriteLine(myGrocceryArray[1]);
-
-            string[] mySecondArray = { "Apples", "Eggs" };
-            Console.WriteLine(mySecondArray[0]);
-            Console.WriteLine(mySecondArray[1]);
-
-            // List
-            List<string> myGroceryList = new List<string>() { "Milk", "Cheese" };
-            myGroceryList.Add("Paneer");
-            foreach (var item in myGroceryList)
-            {
-                Console.WriteLine(item);
-            }
-
-            //IEnumerable
-            IEnumerable<string> myGroceryEnumerable = myGroceryList;
-            Console.WriteLine(myGroceryEnumerable.First());
-
-            //2d Arrays
-            string[,] myTwoDimensionalArray = new string[,]
-            {
-                { "2d-Apples", "2d-Eggs" },
-                { "2d-Milk", "2d-Cheese" }
-            };
-            Console.WriteLine(myTwoDimensionalArray[0, 0]);
-
-            // Dictionary
-            Dictionary<string, string> myGroceryDictionary = new Dictionary<string, string>()
-            {
-                { "dict-String", "dict-Dairy" }
-            };
-            Console.WriteLine(myGroceryDictionary["dict-String"]);
-
-            // Methods
-            Console.WriteLine(GetSum());
+            Computer obj = new Computer();
+            obj.Motherboard = "ROG Zephyrus";
+            Console.WriteLine(obj.Motherboard);
         }
     }
 }
