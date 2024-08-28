89e4a9c4f9d325bf70087423f563b7ff05c18b32
Vishal Rathore <vshl.rathore87@gmail.com>
2024-08-27 20:57:56 -0500

    Using Dapper to read data from the database

--- Program.cs
+++ Program.cs
@@ -26,56 +26,77 @@ namespace dotNetPlayground
             }
 
             // Build Computer object
-            Computer myComputer = new Computer(
-                "ASUS Zephyrus",
-                8,
-                true,
-                false,
-                DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local),
-                1700.00M,
-                "RTX 4070"
-            );
+            // Computer myComputer = new Computer(
+            //     "ASUS Zephyrus",
+            //     8,
+            //     true,
+            //     false,
+            //     DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local),
+            //     1700.00M,
+            //     "RTX 4070"
+            // );
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     connection.Open();
-                    string sqlString =
-                        @"
-                        INSERT INTO TutorialAppSchema.Computer(
-                            Motherboard,
-                            CPUCores,
-                            HasWifi,
-                            HasLTE,
-                            ReleaseDate,
-                            Price,
-                            VideoCard
-                        ) VALUES (
-                            @Motherboard,
-                            @CPUCores,
-                            @HasWifi,
-                            @HasLTE,
-                            @ReleaseDate,
-                            @Price,
-                            @VideoCard
-                        )";
+                    // string sqlString =
+                    //     @"
+                    //     INSERT INTO TutorialAppSchema.Computer(
+                    //         Motherboard,
+                    //         CPUCores,
+                    //         HasWifi,
+                    //         HasLTE,
+                    //         ReleaseDate,
+                    //         Price,
+                    //         VideoCard
+                    //     ) VALUES (
+                    //         @Motherboard,
+                    //         @CPUCores,
+                    //         @HasWifi,
+                    //         @HasLTE,
+                    //         @ReleaseDate,
+                    //         @Price,
+                    //         @VideoCard
+                    //     )";
 
-                    Console.WriteLine(sqlString);
-                    int result = connection.Execute(
-                        sqlString,
-                        new
-                        {
-                            myComputer.Motherboard,
-                            myComputer.CPUCores,
-                            myComputer.HasWifi,
-                            myComputer.HasLTE,
-                            myComputer.ReleaseDate,
-                            myComputer.Price,
-                            myComputer.VideoCard
-                        }
-                    );
-                    Console.WriteLine(result);
+                    // Console.WriteLine(sqlString);
+                    // int result = connection.Execute(
+                    //     sqlString,
+                    //     new
+                    //     {
+                    //         myComputer.Motherboard,
+                    //         myComputer.CPUCores,
+                    //         myComputer.HasWifi,
+                    //         myComputer.HasLTE,
+                    //         myComputer.ReleaseDate,
+                    //         myComputer.Price,
+                    //         myComputer.VideoCard
+                    //     }
+                    // );
+                    // Console.WriteLine(result);
+
+                    string sqlSelect =
+                        @"SELECT  Computer.Motherboard,
+                            Computer.CPUCores,
+                            Computer.HasWifi,
+                            Computer.HasLTE,
+                            Computer.ReleaseDate,
+                            Computer.Price,
+                            Computer.VideoCard 
+                            FROM TutorialAppSchema.Computer";
+                    IEnumerable<Computer> computers = connection.Query<Computer>(sqlSelect);
+                    foreach (Computer computer in computers)
+                    {
+                        Console.WriteLine(computer.Motherboard);
+                        Console.WriteLine(computer.CPUCores);
+                        Console.WriteLine(computer.HasWifi);
+                        Console.WriteLine(computer.ReleaseDate);
+                        Console.WriteLine(computer.Price);
+                        Console.WriteLine(computer.VideoCard);
+                        Console.WriteLine(computer.HasLTE);
+                    }
                 }
                 catch (SqlException ex)
                 {
