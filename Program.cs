e7df77fed7f78ef5bbf4c86089e20880dd068a49
Vishal Rathore <vshl.rathore87@gmail.com>
2024-09-05 16:26:58 -0500

    Added tasks that run asynchronously

--- Program.cs
+++ Program.cs
@@ -2,61 +2,23 @@
 using dotNetPlayground.Data;
 using dotNetPlayground.Models;
 using Microsoft.Extensions.Configuration;
+using static System.Console;
 
 namespace dotNetPlayground
 {
-    public static class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Build a configuration object to read from appsettings.json
-            var config = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            // Initialize the DataContextDapper with the configuration
-            DataContextDapper dataContextDapper = new DataContextDapper(config);
-            DataContextEF entityFramework = new DataContextEF(config);
-
-            // Build Computer object
-            Computer myComputer = new Computer(
-                "ASUS Zephyrus",
-                8,
-                true,
-                false,
-                DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local),
-                1700.00M,
-                "RTX 4070"
-            );
-
-            try
+            Task firstTask = new Task(() =>
             {
-                // int result = dataContextDapper.InsertComputer(myComputer);
-                // Console.WriteLine(result);
+                Thread.Sleep(100);
+                WriteLine("Task 1");
+            });
 
-                //IEnumerable<Computer> computers = dataContextDapper.GetAllComputers();
-                IEnumerable<Computer>? computers = entityFramework.Computer?.ToList<Computer>();
-
-                if (computers != null)
-                {
-                    foreach (Computer computer in computers)
-                    {
-                        Console.WriteLine(computer.ComputerId);
-                        Console.WriteLine(computer.Motherboard);
-                        Console.WriteLine(computer.CPUCores);
-                        Console.WriteLine(computer.HasWifi);
-                        Console.WriteLine(computer.ReleaseDate);
-                        Console.WriteLine(computer.Price);
-                        Console.WriteLine(computer.VideoCard);
-                        Console.WriteLine(computer.HasLTE);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
+            firstTask.Start();
+            WriteLine("After the task was created");
+            await firstTask;
         }
     }
 }
