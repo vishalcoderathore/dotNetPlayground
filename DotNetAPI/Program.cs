7d3737f2d22963fdf721f5b11a86a50601fb7c19
Vishal Rathore <vshl.rathore87@gmail.com>
2024-09-07 18:06:51 -0500

    CORS configuration added

--- DotNetAPI/Program.cs
+++ DotNetAPI/Program.cs
@@ -6,13 +6,53 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+// Setup CORS
+builder.Services.AddCors(
+    (options) =>
+    {
+        options.AddPolicy(
+            "DevCors",
+            (corsBuilder) =>
+            {
+                corsBuilder
+                    .WithOrigins(
+                        "http://localhost:4200",
+                        "http://localhost:3000",
+                        "http://localhost:8000",
+                        "http://localhost:8080"
+                    )
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+        options.AddPolicy(
+            "ProdCors",
+            (corsBuilder) =>
+            {
+                corsBuilder
+                    .WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+    }
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
