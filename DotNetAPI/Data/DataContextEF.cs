3088759615a1f068f4d76addf987e7df2214142c
Vishal Rathore <vshl.rathore87@gmail.com>
2024-09-10 20:30:08 -0500

    Added EntityFrameworkCore

---
+++ DotNetAPI/Data/DataContextEF.cs
@@ -0,0 +1,23 @@
+using DotNetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetAPI.Data;
+
+public class DataContextEF : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public DataContextEF(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<UserJobInfo> UserJobInfo { get; set; }
+    public virtual DbSet<UserSalary> UserSalary { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("TutorialAppSchema");
+    }
+}
